29a4d550185f1135fb16f7ca0c9203bd4e5523a3
bratablessza <bratablessza1997@gmail.com>
2021-03-28 13:17:03 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote local ke online</title>
+</head>
+
+<body>
+    <h2>Brata blessza say hello</h2>
+</body>
+
+</html>
