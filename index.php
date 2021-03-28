b27793a3ab6bce597065684fad69e0bcd3aba85a
bratablessza <bratablessza1997@gmail.com>
2021-03-28 13:25:39 +0700

    nambah list

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 <body>
     <h2>Brata blessza say hello</h2>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti recusandae rerum inventore aspernatur dolores unde ipsa error, modi necessitatibus iste ex soluta dignissimos alias temporibus autem, minus architecto quia nemo!</p>
+    <ul>
+        <li> satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 
 </html>
